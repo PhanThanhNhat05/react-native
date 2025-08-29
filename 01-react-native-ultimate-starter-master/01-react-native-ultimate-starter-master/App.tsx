e1b39501f58174cc137eaa2efb69fd8bf0b78c45
Your Name <you@example.com>
2025-08-30 02:29:32 +0700

    state and data-type

--- 01-react-native-ultimate-starter-master/01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/01-react-native-ultimate-starter-master/App.tsx
@@ -1,13 +1,42 @@
 // import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+          // data type
+  //string
+  const[name, setName] = useState<string>("Ngo Anh Thu");
+  //number
+  const[age, setAge] = useState<number>(18);
+  // null, underfined, boolean
+
+  const test = undefined;
+  //object, array
+
+  const [person, setPerson] = useState<{
+    name: string;
+    age: number;
+  }>({
+    name: "Phan Thanh Nhat",
+    age: 20
+  });
+
+  //array
+  const [numbers, setNumbers] = useState([{
+    name: "Number 1",
+    age1: 25
+  }]);
+
+
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text1}>Nat</Text>
+        <Text style={styles.text1}>{name} : {age}</Text>
+        <Text style={styles.text1}>{person.name} : {person.age}</Text>
+        {/* // convert object sang string */}
+        <Text style={styles.text1}>{JSON.stringify(numbers)}</Text>
       </View>
-      <Text style={styles.text2}>Phan Thanh Nhat</Text>
+      <Text style={styles.text2}>Phan Thanh Nhat : {age}</Text>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
@@ -25,7 +54,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 20,
+    paddingHorizontal: 20,
+
   },
 });
