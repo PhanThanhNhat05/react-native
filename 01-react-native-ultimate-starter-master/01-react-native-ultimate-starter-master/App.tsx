c626d0477575c9dd2abf58b1315446a8d377e444
Your Name <you@example.com>
2025-09-05 00:30:47 +0700

    route params

--- 01-react-native-ultimate-starter-master/01-react-native-ultimate-starter-master/App.tsx
+++ 01-react-native-ultimate-starter-master/01-react-native-ultimate-starter-master/App.tsx
@@ -1,7 +1,7 @@
 // import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useRoute } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
 
@@ -10,48 +10,55 @@ export default function App() {
 
   const Stack = createNativeStackNavigator();
 
-  function HomeScreen(props: any) {
-    console.log("props", props);
-    const navigation = props.navigation;
-    // const navigation:any = useNavigation(); (Hooks)cách này nó di động hơn cách khai báo props
+  function HomeScreen() {
+    // console.log("props", props);
+    // console.log("props route", props.route);
+    // const navigation = props.navigation;
+    
+    const navigation:any = useNavigation(); 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      
       <View style={{marginVertical: 20}}>
         <Button 
       title='Go to detail' 
-      onPress={() => navigation.navigate('Details')}
+      onPress={() => navigation.navigate('nat')}
       />
       </View>
       <View style={{marginVertical: 20}}>
         <Button 
       title='Go to user id 1' 
-      onPress={() => navigation.navigate('Details')}
+      onPress={() => navigation.navigate('nat', {userId: 1, name: 'nat1'})}
       />
       </View>
       <View style={{marginVertical: 20}}>
         <Button 
       title='Go to user id 2' 
-      onPress={() => navigation.navigate('Details')}
+      onPress={() => navigation.navigate('nat', {userId: 2, name: 'nat2'})}
       />
       </View>
     </View>
   );
 }
-  function DetailsScreen(props: any) {
-    const navigation = props.navigation;
+  function DetailsScreen() {
+    const navigation:any = useNavigation();
+    const route:any = useRoute();
+    console.log(">> check route", route.params);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>User id: {route.params?.userId}</Text>
       <Button title='Go back Home' onPress={() => navigation.goBack("Home")} />
     </View>
   );
 }
+
   return (
     <NavigationContainer> 
 <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="nat" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
